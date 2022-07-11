1d126d68da2acc3f1dc7c42f22662fa2ddb075ab
Mateus Castellar <mateuscastellar@outlook.com>
2022-07-10 21:04:05 -0300

    feat: Controllers para exibir pedidos e eventos

---
+++ src/DDD.WebApp.Mvc/Controllers/EventosController.cs
@@ -0,0 +1,24 @@
+﻿using DDD.Core.Data.EventSourcing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DDD.WebApp.Mvc.Controllers
+{
+    //controller de teste para vizualizar a persistencia de eventos
+
+    public class EventosController : Controller
+    {
+        private readonly IEventSourcingRepository _eventSourcingRepository;
+
+        public EventosController(IEventSourcingRepository eventSourcingRepository)
+        {
+            _eventSourcingRepository = eventSourcingRepository;
+        }
+
+        [HttpGet("eventos/{id:guid}")]
+        public async Task<IActionResult> Index(Guid id)
+        {
+            var eventos = await _eventSourcingRepository.ObterEventos(id);
+            return View(eventos);
+        }
+    }
+}
---
+++ src/DDD.WebApp.Mvc/Controllers/PedidoController.cs
@@ -0,0 +1,24 @@
+﻿using DDD.Core.Communication.Mediator;
+using DDD.Core.Messages.CommonMessages.Notifications;
+using DDD.Vendas.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DDD.WebApp.Mvc.Controllers
+{
+    public class PedidoController : CoreController
+    {
+        private readonly IPedidosQueries _pedidoQueries;
+
+        public PedidoController(IPedidosQueries pedidoQueries,
+                                INotificationHandler<DomainNotification> notifications,
+                                IMediatorHandler mediatorHandler) : base(notifications, mediatorHandler)
+        {
+            _pedidoQueries = pedidoQueries;
+        }
+
+        [Route("meus-pedidos")]
+        public async Task<IActionResult> Index() => View(await _pedidoQueries.ObterPedidosCliente(ClienteId));
+
+    }
+}
